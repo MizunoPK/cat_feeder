730aeb957f4e95dcf8c32d0b6f3e012882dfbd7a
Kai Mizuno <Mizuno.PK@gmail.com>
2024-10-24 13:17:28 -0600

    updated todo

--- data/SortPictures.py
+++ data/SortPictures.py
@@ -2,7 +2,7 @@ import os
 import cv2
 import shutil
 
-img_folder = "./data/CatPics/unsorted"
+img_folder = "./data/SinglePhotos/yarn"
 nori_folder = "./data/CatPics/nori"
 bento_folder = "./data/CatPics/bento"
 
@@ -11,7 +11,7 @@ img_list = os.listdir(img_folder)
 for img_name in img_list:
     img_full_path = os.path.join(img_folder, img_name)
     img = cv2.imread(img_full_path)
-    img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
+    # img = cv2.resize(img, (0,0), fx=0.5, fy=0.5)
     cv2.imshow("output", img)
     cv2.waitKey(1)
     selection = input("Nori (1), Bento (2) or Trash (3): ")
